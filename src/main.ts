9648f1bd4b3995a0a7d7470cbe7c6d9caea41f30
Rachid Echafai <rachidchaf2001@gmail.com>
2024-11-16 13:51:06 +0100

    new dummy commit 13

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,4 @@ async function bootstrap() {
     process.exit(1);
   }
 }
-
-async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT || 3000);
-    console.log(`Application is running on: ${await app.getUrl()}`);
-  } catch (error) {
-
-    console.error('Application failed to start:', error);
-    process.exit(1);
-  }
-}
 bootstrap();
